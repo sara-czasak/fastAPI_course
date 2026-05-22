4ed9de58d632e3edd6ba3cbeda73d8842649a101
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-05-22 17:35:32 +0200

    add a list of books

--- books_project/books.py
+++ books_project/books.py
@@ -5,6 +5,15 @@ app = FastAPI()
 # To start app cd into dir then: uvicorn app_name:app --reload
 
 
-@app.get('/api-endpoint')
-async def first_api():
-    return {'message': 'Hello, Sara!'}
+BOOKS = [
+    {'title': 'The secret of the old clock', 'author': 'Carolyn Keene', 'category': 'adventure'},
+    {'title': 'The hidden staircase', 'author': 'Carolyn Keene', 'category': 'adventure'},
+    {'title': 'Halt\'s Peril', 'author': 'John Flanagan', 'category': 'adventure'},
+    {'title': 'Fourth Wing', 'author': 'Rebecca Yarros', 'category': 'fantasy'},
+    {'title': 'Iron Flame', 'author': 'Rebecca Yarros', 'category': 'fantasy'},
+]
+
+
+@app.get('/books')
+async def read_all_books():
+    return BOOKS
