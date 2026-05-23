b3b8ea921940ea5057cbb8973af3b6ae948cc1db
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-05-23 08:42:52 +0200

    add Book object

--- books_project/books2.py
+++ books_project/books2.py
@@ -1,13 +1,39 @@
+import string
+
 from fastapi import FastAPI
 
 
 app = FastAPI()
 
 
-BOOKS = []
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+
+BOOKS = [
+    Book(1, "Computer Science Pro", "Coding with Roby", "A great book", 5),
+    Book(2, "Be fast with FastAPI", "Coding with Roby", "A very nice book", 5),
+    Book(3, "Master Endpoints", "Coding with Roby", "An awesome book", 5),
+    Book(4, "HP1", "Author 1", "Book description", 2),
+    Book(5, "HP2", "Author 2", "Book description", 3),
+    Book(6, "HP3", "Author 3", "Book description", 1),
+]
 
 
 @app.get('/books')
 async def read_books():
     """Get all books"""
     return BOOKS
+
+
