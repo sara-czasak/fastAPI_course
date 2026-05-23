5e275833b3916ddadd398fd0a857d00f34830f46
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-05-23 13:31:35 +0200

    start setting up db

---
+++ TodoApp/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+# URL to create location of database for application
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+
+# Create db engine (allow only one thread to connect to db to prevent accidental sharing of the same connection)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+# Create a session local
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create an object of the db to be able to control db later
+Base = declarative_base()
+
