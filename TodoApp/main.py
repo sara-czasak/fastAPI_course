c0bfdaf9b3a38b157edff1ed34d1a9476048d2c1
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-05-23 15:16:18 +0200

    add data validation and post endpoint

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -5,6 +5,7 @@ from database import engine, SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
 from starlette import status
+from pydantic import BaseModel, Field
 
 
 app = FastAPI()
@@ -24,6 +25,14 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+# Create pydantic request
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+
 @app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
@@ -35,5 +44,13 @@ async def get_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail="Todo not found")
+
+
+@app.post('/todo', status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+    db.add(todo_model)
+    db.commit()
+
 
 
