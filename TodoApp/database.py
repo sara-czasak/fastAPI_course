f2dd82deb1767d6f937c10a96f1f79da2b7a582a
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-05-24 04:47:31 +0200

    add user model

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 # URL to create location of database for application
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 
 # Create db engine (allow only one thread to connect to db to prevent accidental sharing of the same connection)
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,16 +1,33 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+
+class Users(Base):
+    # Table name
+    __tablename__ = "users"
+
+    # Columns
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 
 
 class Todos(Base):
-    # Name of the db
+    # Table name
     __tablename__ = 'todos'
 
-    # Create columns
+    # Columns
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    # Key referencing a different table
+    owner_id = Column(Integer, ForeignKey('users.id'))
 
 
